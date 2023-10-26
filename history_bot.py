6aaa8b3720deeabc9e03607f97bf3e8d65ff5121
Danil <workdanilput@mail.ru>
2023-10-26 15:47:11 +0300

    Edit main_send

--- history_bot.py
+++ history_bot.py
@@ -1,6 +1,6 @@
 import telebot, function
 
-bot = telebot.TeleBot("") #TOKEN BOT
+bot = telebot.TeleBot("6480240193:AAFsuxAPea8LfE_3gRnwH8Eo_zeD038VNc0")
 
 @bot.message_handler(commands=["start"])
 def welcome(message):
